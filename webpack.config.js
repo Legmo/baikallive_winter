78fe914126eba2e1111ba04b7c4833b3fa2b8af4
Vladimir Degtiarev <vladimir@ownstr.com>
2018-01-08 17:09:34 +0800

    Frontend fixes

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,14 @@ const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-   
    //где лежат исходные файлы (точки входа)
-   context: __dirname + '/src',
-   entry: {
-      home:  "./main.js",
-      //about: "./about"
-    },        
-    
+    context: __dirname + '/src',
+    entry: {
+        home:  "./main.js",
+        //about: "./about",
+        //home:  ["../node_modules/webpack-dev-server/client", "../node_modules/webpack/hot/dev-server", "./main.js"],
+    },
+
     //куда компилируется финальный файл
     output: {
         path:     __dirname + '/final',
@@ -27,7 +27,6 @@ module.exports = {
 
     //автоматически отслеживаем изменения
     watch: true,
-    
     watchOptions: {
       aggregateTimeout: 100
     },
@@ -56,9 +55,31 @@ module.exports = {
                 {
                   loader: 'style-loader', // inject CSS to page
                 },
-
+                //Section for Webpack-dev-server (develoemnt only).
+                /*{ loader: 'css-loader',
+                  options: {
+                      sourceMap: true
+                  }
+                },
+                { loader: 'postcss-loader',
+                  options: {
+                      ident: 'postcss',
+                      plugins: (loader) => [
+                          require('precss'),
+                          require('autoprefixer')('ie >= 9', 'last 2 version')
+                      ],
+                      sourceMap: true
+                    }
+                },
+                {  loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }
+                */
               ]
             },
+            //Disable this section at Webpack-dev-server mode. For css hot reload
             {test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
@@ -89,12 +110,12 @@ module.exports = {
             },
             { test: /\.(png|jpg|svg)$/,
             use: [{
-            loader: 'file-loader?name=[name].[ext]&outputPath=img/',
+            loader: 'file-loader?name=[name].[ext]?[hash]&outputPath=img/',
             }],
             },
             { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
             use: [{
-                  loader: 'url-loader?name=[name].[ext]&outputPath=fonts/',
+                  loader: 'url-loader?name=[name].[ext]?[hash]&outputPath=fonts/',
                   options: {
                       limit: 10000,
                       mimetype: 'application/font-woff'
@@ -104,16 +125,24 @@ module.exports = {
             { test: /\.(ttf|eot|otf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
             exclude: [/img/],
             use: [{
-              loader: 'file-loader?name=[name].[ext]&outputPath=fonts/'
+              loader: 'file-loader?name=[name].[ext]?[hash]&outputPath=fonts/'
             }],
             },
-      ]
+        ]
     },
 
     //Plugins
     plugins: [
+        //new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin(),
-        new ExtractTextPlugin('./css/[name].css', {allChunks: true}),
-        //new ExtractTextPlugin('./css/[name].css', {allChunks: true})
+        new ExtractTextPlugin('./css/[name].css', {allChunks: true}), //disable this line at Webpack-dev-server mode. For css hot reload
     ],
+
+    //Webpack-dev-server
+    devServer: {
+        contentBase: __dirname + '/final',
+        //hot: true,
+        //inline: true,
+    },
+
 }
